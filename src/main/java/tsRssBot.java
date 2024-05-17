da784b850ecb35b634c9394da3250b88088b4345
Carlo <luckyginn@pm.me>
2024-05-17 22:49:34 +0200

    - So much refactoring

--- src/main/java/tsRssBot.java
+++ src/main/java/tsRssBot.java
@@ -1,4 +1,3 @@
-import bot.LoginBot;
 import bot.botCommandsHandler;
 import com.github.theholywaffle.teamspeak3.TS3Api;
 import com.github.theholywaffle.teamspeak3.TS3Config;
@@ -9,16 +8,24 @@ public class tsRssBot {
     public static void main(String[] args) {
 
         String ipAddress = "nocars.tk";
+        String username = "serveradmin";
+        String password = "2jtwh++y";
+        String nickname = "LeBot";
+        String arrivalMessage = nickname + " is online!";
+
         TS3Config config = new TS3Config();
         config.setHost(ipAddress);
         config.setEnableCommunicationsLogging(true);
 
+        //Connect and Login Bot
         TS3Query query = new TS3Query(config);
         query.connect();
 
         TS3Api api = query.getApi();
-        LoginBot bot = new LoginBot(api);
-        bot.connect();
+        api.login(username, password);
+        api.selectVirtualServerById(1);
+        api.setNickname(nickname);
+        api.sendChannelMessage(arrivalMessage);
 
         //Handle incoming commands
         botCommandsHandler botCommandsHandler = new botCommandsHandler(api, query);
