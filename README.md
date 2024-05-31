08a755db2bf219c86c462de571f67818120d9e80
Carlo <hi@nocars.tk>
2024-05-31 16:43:20 +0200

    Started working on saving commands in Yaml for automatic reuse

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 - [x] Mit .rss Dateien arbeiten
 - [x] HTML Websites mit Bot Command fetchen
 - [x] Login zum Server mit YAML / Environment Variables
-- [ ] ID schöner im Chat ausgeben
+- [x] ID schöner im Chat ausgeben
 - [ ] Regelmäßiges aktualisieren der Quellen
 - [ ] Tutorial link für !editHTML
--- src/main/java/YamlHandler.java
+++ src/main/java/YamlHandler.java
@@ -4,9 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 public class YamlHandler {
+
+    Yaml yaml = new Yaml();
+    File file = new File("src/main/resources/config.yaml");
 
     private String ipAddress;
     private String username;
@@ -14,8 +18,8 @@ public class YamlHandler {
     private String nickname;
 
     public void getYaml () {
-        Yaml yaml = new Yaml();
-        File file = new File("src/main/resources/config.yaml");
+        //Yaml yaml = new Yaml();
+        //File file = new File("src/main/resources/config.yaml");
         try {
             InputStream fis = new FileInputStream(file);
             Map<String, Object> configyaml = yaml.load(fis);
@@ -45,5 +49,21 @@ public class YamlHandler {
 
     public String getNickname() {
         return nickname;
+    }
+
+    public void addNews (String id, String parameters) {
+        Map<String, Object> news = new HashMap<>();
+        news.put(id, parameters);
+        yaml.dump(news);
+    }
+
+    public void testDump() {
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("name", "Silenthand Olleander");
+        data.put("race", "Human");
+        data.put("traits", new String[] { "ONE_HAND", "ONE_EYE" });
+        Yaml yaml = new Yaml();
+        String output = yaml.dump(data);
+        System.out.println(output);
     }
 }
